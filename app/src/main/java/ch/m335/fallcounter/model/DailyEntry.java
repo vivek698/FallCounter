5413f44cfbbfb826dd7963765bee20329e40deea
vivvir <viruthiyelv@bzz.ch>
2023-07-06 11:22:22 +0200

    DailyEntry finshed

--- app/src/main/java/ch/m335/fallcounter/model/DailyEntry.java
+++ app/src/main/java/ch/m335/fallcounter/model/DailyEntry.java
@@ -1,3 +1,29 @@
 package ch.m335.fallcounter.model;
+
+import java.util.Date;
+
 public class DailyEntry {
+    private int counter;
+    private Date date;
+
+    public DailyEntry(int counter, Date date) {
+        this.counter = counter;
+        this.date = date;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
